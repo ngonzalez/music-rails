4f19d696f713c43511824b3a7ae16e7ecfc7326f
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2015-05-16 21:18:00 -0400

    Minor change

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -51,7 +51,7 @@ class MusicController < ApplicationController
 
   def find_tracks
     @tracks = Track.search(include: [:release]) { fulltext params[:q] ; paginate :page => params[:page], :per_page => params[:per_page] }.hits.each_with_object({}) do |hit, hash|
-      hash[hit.result.release_id] = { name: hit.result.release.name, tracks: [] } if !hash.has_key?(hit.result.release_id)
+      hash[hit.result.release_id] = { name: hit.result.release.name, url: music_url(hit.result.release, format: :json), tracks: [] } if !hash.has_key?(hit.result.release_id)
       hash[hit.result.release_id][:tracks] << hit.result.attributes.merge(score: hit.score, path: hit.result.file_url)
     end
   end
