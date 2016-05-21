5eba499f13908dc9e87e2d897d3d8c63bc8a16e5
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2016-05-21 22:11:23 +0200

    Fix Path

--- app/decorators/release_decorator.rb
+++ app/decorators/release_decorator.rb
@@ -1,10 +1,10 @@
 class ReleaseDecorator < Draper::Decorator
   delegate_all
   def path
-    [BASE_URL, self.folder, self.name].join("/")
+    [BASE_URL, self.folder, self.label_name.gsub(" ","_"), self.read_attribute(:source), self.name].reject(&:blank?).join("/")
   end
   def search_infos
-    object.attributes.deep_symbolize_keys.slice(:formatted_name, :folder, :format_name, :label_name).merge(
+    object.attributes.deep_symbolize_keys.slice(:id, :formatted_name, :folder, :format_name, :label_name).merge(
       url: h.music_url(object, format: h.params.slice("format")), year: object.year.to_i
     )
   end
