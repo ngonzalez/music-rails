1443d3a02069f9f0c550192ecf1800aaf6c60ab3
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2016-06-04 13:23:41 +0200

    Update Upload Section

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(layout.css music.js music.css)
+Rails.application.config.assets.precompile += %w(layout.css music.js music.css upload.js)
 
 ::Sass.load_paths << Rails.root.join('app/assets/stylesheets/')
