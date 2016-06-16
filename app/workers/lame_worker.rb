37c8095c475817070f724a288da93b074b237910
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2016-06-16 19:47:05 +0200

    Update Worker

--- app/workers/lame_worker.rb
+++ app/workers/lame_worker.rb
@@ -5,29 +5,42 @@ class LameWorker
   sidekiq_options :queue => :default, :retry => false, :backtrace => true
 
   def perform track_id
-    track = Track.find track_id
+    track = nil
+    loop do
+      begin
+        track = Track.find track_id
+      rescue
+        Rails.logger.info "\sLameWorker: DB Busy, Retrying.."
+        sleep 1
+      end
+      break if !track.nil?
+    end
     begin
       if !track.file
         file_path = [BASE_PATH, track.release.decorate.path, track.name].join("/")
-        if track.format_name =~ /MP3/
+        if ["iTunes AAC"].include?(track.format_name) || track.format_name =~ /MP3/
           track.update! file: File.open(file_path)
-        else
+        elsif ["FLAC"].include? track.format_name
           file_path = Shellwords.escape file_path
-          if track.format_name == "FLAC"
-            temp_file_flac = "/tmp/#{track.id}.flac"
-            temp_file_wav = "/tmp/#{track.id}.wav"
-            copy_file file_path, temp_file_flac
-            decode_file temp_file_flac
-          end
+          temp_file_flac = "/tmp/#{track.id}.flac"
+          temp_file_wav = "/tmp/#{track.id}.wav"
+          copy_file file_path, temp_file_flac
+          decode_file temp_file_flac
           temp_file = "/tmp/#{track.id}.#{DEFAULT_ENCODING}"
-          encode temp_file_wav || file_path, temp_file
+          encode temp_file_wav, temp_file
+          track.update! file: File.open(temp_file)
+        elsif ["WAV", "DATA", "FLAC"].include? track.format_name
+          file_path = Shellwords.escape file_path
+          temp_file = "/tmp/#{track.id}.#{DEFAULT_ENCODING}"
+          encode file_path, temp_file
           track.update! file: File.open(temp_file)
         end
       end
     rescue Exception => e
-      puts e.inspect
+      Rails.logger.info e.inspect
+      raise e
     ensure
-      track.update! process_id: nil
+      track.update! process_id: nil if track.file
       FileUtils.rm_f temp_file if temp_file && File.exists?(temp_file)
       FileUtils.rm_f temp_file_wav if temp_file_wav && File.exists?(temp_file_wav)
       FileUtils.rm_f temp_file_flac if temp_file_flac && File.exists?(temp_file_flac)
@@ -37,7 +50,7 @@ class LameWorker
   def encode source, destination
     case DEFAULT_ENCODING
       when "mp3"
-        `lame -S -V0 #{source} #{destination}`
+        `lame -S -b 320 -ms #{source} #{destination}`
       when "aac"
         `ffmpeg -i #{source} -c:a aac -strict -2 #{destination}`
     end
