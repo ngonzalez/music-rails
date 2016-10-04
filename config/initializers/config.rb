2c542f1ff3874b6b655a2a995d7e988eb93b5765
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2016-10-04 20:03:22 +0200

    Update Config

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,16 +1,15 @@
-# BASE_PATH = "/Volumes/FreeAgent\ GoFlex\ Drive/music"
-BASE_PATH = "/Volumes/Seagate\ Backup\ Plus\ Drive/music"
+BASE_PATH = "/Volumes/My\ Drive/music"
 ALLOWED_METRICS = ["format_name", "genre", "year", "bitrate"]
-ALLOWED_AUDIO_FORMATS = ["mp3", "mp4", "m4a", "flac", "wav", "aiff"]
-ALLOWED_IMAGE_FORMATS = ["jpg", "JPG", "jpeg", "JPEG", "gif", "png", "tiff", "bmp"]
-ALLOWED_SOURCES = ["WEB", "Vinyl", "CD", "SAT-CABLE", "Line", "Live", "Tape"]
+ALLOWED_AUDIO_FORMATS = ["mp3", "mp4", "m4a", "flac", "wav", "aiff", "aif"]
+ALLOWED_IMAGE_FORMATS = ["jpg", "JPG", "jpeg", "JPEG"]
+ALLOWED_SOURCES = ["WEB", "Vinyl", "CD", "Live", "TAPE"]
 NFO_TYPE = "nfo"
 SFV_TYPE = "sfv"
 SFV_CHECK_APP = "cfv"
 DEFAULT_ENCODING = "mp3"
-LABELS = []
 HOST_NAME = "example.net"
 BITLY_TOKEN = "REPLACE_TOKEN"
-FOLDERS = ["dnb","hc","other"]
-FOLDERS_WITH_LABELS = ["backup"]
-EXCEPT_GRPS = /EXAMPLE_INT|ABCD_INT/
+FOLDERS = ["music"]
+FOLDERS_WITH_SUBFOLDERS = ["backup"]
+EXCEPT_GRPS = EXCEPT_GRPS = /EXAMPLE/
+EXCEPT_RLS = []
