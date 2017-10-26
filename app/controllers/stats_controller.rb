8c45bd4df150079f421684fadc219998ecb62575
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2017-10-26 20:34:17 +0200

    Update stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,12 +1,12 @@
 class StatsController < ApplicationController
   def index
     set_stats_params
-    @metrics1 = get_values @metric1
-    @metrics2 = get_values @metric2 
+    @metrics1 = get_values @m1
+    @metrics2 = get_values @m2
     @stats = get_stats
     respond_to do |format|
       format.html do
-        @data = format_data
+        @data = format_data.sort_by &:first
       end
       format.json do
         render json: @stats.to_json
@@ -17,14 +17,14 @@ class StatsController < ApplicationController
   private
 
   def set_stats_params
-    @metric1 = params[:m1] && ALLOWED_METRICS.include?(params[:m1]) ? params[:m1] : ALLOWED_METRICS[0]
-    @metric2 = params[:m2] && ALLOWED_METRICS.include?(params[:m2]) ? params[:m2] : ALLOWED_METRICS[0]
+    @m1 = params[:m1] && ALLOWED_METRICS.include?(params[:m1]) ? params[:m1] : ALLOWED_METRICS[0]
+    @m2 = params[:m2] && ALLOWED_METRICS.include?(params[:m2]) ? params[:m2] : ALLOWED_METRICS[0]
   end
 
   def format_data
     @metrics2.each_with_object([]) do |metric2, data_array|
       stats = @metrics1.each_with_object([]) do |name, array|
-        stat_infos = @stats[metric2].detect{|item| item[@metric1] == name }
+        stat_infos = @stats[metric2].detect { |item| item[@m1] == name }
         array << (stat_infos ? stat_infos["count"].to_i : 0)
       end
       data_array << [metric2.to_s] + stats
@@ -32,8 +32,8 @@ class StatsController < ApplicationController
   end
 
   def get_stats
-    @metrics2.each_with_object({}) do |metric, hash|
-      hash[metric] = find_releases(metric).to_hash
+    @metrics2.each_with_object({}) do |value, hash|
+      hash[value] = find_tracks(value).to_hash
     end
   end
 
@@ -41,25 +41,34 @@ class StatsController < ApplicationController
     Track.pluck(metric).uniq.map &:to_s
   end
 
-  def find_releases m2_value
-    if m2_value.to_i > 0
-      m2_condition = "#{@metric2}::numeric = #{m2_value.to_i}"
-    else
-      m2_condition = "#{@metric2} = '#{m2_value.gsub("'", "")}'"
-    end
+  def find_tracks value
     sql_base = <<-SQL
-      SELECT t1.#{@metric1}, (
+      SELECT t1.#{@m1}, (
         SELECT COUNT(t2.id)
-        FROM tracks t2
-        WHERE t1.#{@metric1} = t2.#{@metric1}
-        AND t2.#{m2_condition}
+        FROM #{Track.table_name} t2
+        WHERE t1.#{@m1} = t2.#{@m1}
+        AND t2.#{tracks_condition(@m2, value)}
       ) AS count
-      FROM tracks t1
-      WHERE t1.#{m2_condition}
-      GROUP BY t1.#{@metric1}
+      FROM #{Track.table_name} t1
+      WHERE t1.#{tracks_condition(@m2, value)}
+      GROUP BY t1.#{@m1}
       ORDER BY 1;
     SQL
     ActiveRecord::Base.connection.select_all sql_base
+  end
+
+  def releases_condition metric, value
+    if %w(created_at_year).include? metric
+      "to_char('folder_created_at', 'YYYY')::numeric = %s" % [value.to_i]
+    end
+  end
+
+  def tracks_condition metric, value
+    if %w(year).include? metric
+      "%s::numeric = %s" % [metric, value.to_i]
+    elsif %w(format_name genre).include? metric
+      "%s = '%s'" % [metric, value.gsub("'", "")]
+    end
   end
 
 end
