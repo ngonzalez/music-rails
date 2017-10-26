4e0b4863d1c4c64c2383ea169e204283f875c421
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2017-10-26 20:33:57 +0200

    Update config

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,8 +1,8 @@
 BASE_PATH = "/Volumes/My\ Drive/music"
-ALLOWED_METRICS = ["format_name", "genre", "year", "bitrate"]
+ALLOWED_METRICS = ["format_name", "genre", "year"]
 ALLOWED_AUDIO_FORMATS = ["mp3", "mp4", "m4a", "flac", "wav", "aiff", "aif"]
 ALLOWED_IMAGE_FORMATS = ["jpg", "JPG", "jpeg", "JPEG"]
-ALLOWED_SOURCES = ["WEB", "Vinyl", "CD", "Live", "TAPE"]
+ALLOWED_SOURCES = ["WEB", "Vinyl", "CD", "Live", "TAPE", "FLASH"]
 NFO_TYPE = "nfo"
 SFV_TYPE = "sfv"
 M3U_TYPE = "m3u"
