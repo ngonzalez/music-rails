9ceb19a524ac64361998ed709699a52231cf0cc8
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2019-12-28 19:16:49 +0100

    Update player JS

--- app/assets/javascripts/music/player.js
+++ app/assets/javascripts/music/player.js
@@ -8,31 +8,23 @@ function init_players(tracks, css) {
     }
     var intervals = {};
     function processing(element, data) {
-        if (!intervals[data.id]) {
-            toggleIcon(element, css.PROCESSING);
-            intervals[data.id] = setInterval(function() {
-                $.get(data.url, function(response) {
-                    if (response.stream_url) {
-                        tracks[data.id].stream_url = response.stream_url;
-                        toggleIcon(element, css.PROCESSING);
-                        clearInterval(intervals[data.id]);
-                        window.location = response.stream_url;
-                    }
-                });
-            }, 2000);
-        }
+        toggleIcon(element, css.PROCESSING);
+        intervals[data.id] = setInterval(function() {
+            $.get(data.url, function(response) {
+                if (response.stream_url) {
+                    toggleIcon(element, css.PROCESSING);
+                    clearInterval(intervals[data.id]);
+                    window.location = response.stream_url;
+                }
+            });
+        }, 2000);
     }
     function observe() {
         if (!tracks) return;
         $.each($('.play-btn'), function(i, element) {
             var data = tracks[parseInt($(element).data('id'))]
             $(element).click(function(e) {
-                window.current_file = data.id;
-                if (data.stream_url) {
-                    window.location = data.stream_url;
-                } else {
-                    processing($(element), data);
-                }
+                processing($(element), data);
             });
         });
     }
