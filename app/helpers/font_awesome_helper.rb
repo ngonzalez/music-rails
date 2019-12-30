29986f4d922bd5d830cf9f4d3de39c6f1dbad5b2
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2019-12-30 21:50:24 +0100

    Update JS, helpers: Minor changes

--- app/helpers/font_awesome_helper.rb
+++ app/helpers/font_awesome_helper.rb
@@ -2,12 +2,12 @@ module FontAwesomeHelper
   def fa_css
     {
       :hidden     => 'hidden',
-      :play_btn   => 'fa-google-play',
+      :play       => 'fa-google-play',
       :processing => 'fa-cog',
     }
   end
   def fa_play_btn data
-    content_tag :i, nil, class: "fab fa-3 %s" % fa_css[:play_btn], data: data
+    content_tag :i, nil, class: "fab fa-3 %s" % fa_css[:play], data: data
   end
   def fa_processing_btn
     content_tag :i, nil, class: "fas fa-3 fa-spin %s %s" % [fa_css[:processing], fa_css[:hidden]]
