2bf43e0342c25a3d4756686db93691ba4d4ef446
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-01-02 00:43:53 +0100

    Update config

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,6 +1,24 @@
-BASE_PATH               = "/Volumes/External\ Drive"
-HLS_FOLDER              = "/tmp/hls"
-HOST_NAME               = "example.com"
+# BASE_PATH
+if ENV['BASE_PATH'].present?
+  BASE_PATH = ENV['BASE_PATH']
+else
+  raise "Missing ENV BASE_PATH"
+end
+
+# HLS_FOLDER
+if ENV['HLS_FOLDER'].present?
+  HLS_FOLDER = ENV['HLS_FOLDER']
+else
+  raise "Missing ENV HLS_FOLDER"
+end
+
+# HOST_NAME
+if ENV['HOST_NAME'].present?
+  HOST_NAME = ENV['HOST_NAME']
+else
+  raise "Missing ENV HOST_NAME"
+end
+
 ALLOWED_AUDIO_FORMATS   = YAML.load_file Rails.root.join('config/yaml/allowed_audio_formats.yaml')
 ALLOWED_IMAGE_FORMATS   = YAML.load_file Rails.root.join('config/yaml/allowed_image_formats.yaml')
 ALLOWED_SOURCES         = YAML.load_file Rails.root.join('config/yaml/allowed_sources.yaml')
--- environment.sh
+++ environment.sh
@@ -1,3 +1,9 @@
+
+export BASE_PATH='/Volumes/ExternalDrive'
+export HLS_FOLDER='/tmp/hls'
+export HOST_NAME='example.com'
+
+# Redis
 export REDIS_HOST='127.0.0.1'
-export REDIS_PORT=6379
+export REDIS_PORT='6379'
 export REDIS_DB='0'
