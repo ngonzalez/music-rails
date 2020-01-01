3a0dc9f55b0d6bfb9fcca0df419522e7d74c9470
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-01-01 08:47:11 +0100

    Add cache store initializer

---
+++ config/initializers/cache_store.rb
@@ -0,0 +1,5 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.cache_store = :redis_store, {
+  cluster: %w["redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/#{ENV['REDIS_DB']}"]
+}
