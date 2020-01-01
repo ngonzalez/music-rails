1e5a8f4cfbae6cbd1a93746fc69bc8779fa274fc
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-01-01 17:13:14 +0100

    Helpers: Minor changes

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -3,9 +3,9 @@ module TracksHelper
     { "data-transition" => "none" }
   end
   def search_terms_array
-    search_params[:q].split(/ and | or /) if search_params[:q]
+    @search_terms_array ||= search_params[:q].split(/ and | or /) if search_params[:q]
   end
   def authorized_search_params
-    search_params.slice :q, :folder, :subfolder
+    @authorized_search_params ||= search_params.slice :q, :folder, :subfolder
   end
 end
