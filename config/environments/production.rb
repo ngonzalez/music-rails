a442c7c3ce2b4d2d96e0bd201455263508c89d2e
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-01-03 14:34:44 +0100

    Move redis cache config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,15 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  config.cache_store = :redis_store, {
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    db: REDIS_DB,
+    namespace: 'cache',
+  }, {
+    expires_in: 90.minutes
+  }
+
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "music_production"
