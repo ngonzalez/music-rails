117abb38d8406eb5f22d5af96d6fc3a4b4409203
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-01-03 17:19:53 +0100

    Search: Remove text highlight

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -2,10 +2,7 @@ module TracksHelper
   def default_transition
     { "data-transition" => "none" }
   end
-  def search_terms_array
-    @search_terms_array ||= search_params[:q].split(/ and | or /) if search_params[:q]
-  end
   def authorized_search_params
-    @authorized_search_params ||= search_params.slice :q, :folder, :subfolder
+    search_params.slice :q, :folder, :subfolder
   end
 end
