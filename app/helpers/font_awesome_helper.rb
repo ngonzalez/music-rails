8027c2a93babc7a5af296bc9ee120a50f2f2ca5f
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-01-05 20:46:01 +0100

    Tracks: Hover rows

--- app/helpers/font_awesome_helper.rb
+++ app/helpers/font_awesome_helper.rb
@@ -1,13 +1,14 @@
 module FontAwesomeHelper
   def fa_css
     {
+      :disabled   => 'disabled',
       :hidden     => 'hidden',
       :play       => 'fa-google-play',
       :processing => 'fa-cog',
     }
   end
   def fa_play_btn data
-    content_tag :i, nil, class: "fab fa-3 %s" % fa_css[:play], data: data
+    content_tag :i, nil, class: "fab fa-3 %s %s" % [fa_css[:play], fa_css[:disabled]], data: data
   end
   def fa_processing_btn
     content_tag :i, nil, class: "fas fa-3 fa-spin %s %s" % [fa_css[:processing], fa_css[:hidden]]
