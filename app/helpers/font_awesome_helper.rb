b2f328a138574929bceb0f7aedd858f61621c4e1
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-01-05 21:52:37 +0100

    Views: Clean helpers

--- app/helpers/font_awesome_helper.rb
+++ app/helpers/font_awesome_helper.rb
@@ -7,10 +7,4 @@ module FontAwesomeHelper
       :processing => 'fa-cog',
     }
   end
-  def fa_play options={}
-    content_tag :i, nil, options.merge(class: "fab fa-3 %s" % fa_css[:play])
-  end
-  def fa_processing options={}
-    content_tag :i, nil, options.merge(class: "fas fa-3 fa-spin %s hidden" % fa_css[:processing])
-  end
 end
