9ebf93ad31b6ba5f9e82d025b6b6b5315bcfbeba
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-01-07 22:36:53 +0100

    List tracks: Minor changes

--- app/decorators/list_decorator.rb
+++ app/decorators/list_decorator.rb
@@ -1,7 +1,7 @@
 class ListDecorator < Draper::Decorator
   def file_names
     files = File.read(file.path).split("\n")
-    files = files.reject { |line| line =~ /^#/ }.reject(&:blank?)
+    files = files.reject { |line| line =~ /^#|^;/ }.reject(&:blank?)
     files = files.grep(/#{ALLOWED_AUDIO_FORMATS.flat_map { |_, format| format[:extensions] }.join("|")}/)
     files = files.map(&:strip).collect { |line| line.split(' ')[0] }
     files
