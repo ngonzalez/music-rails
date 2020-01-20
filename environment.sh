691f6d83ff17aa24c0dcae7f396d6238ad4cfd8b
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-01-20 22:04:10 +0100

    Update config

--- environment.sh
+++ environment.sh
@@ -3,7 +3,19 @@ export BASE_PATH='/Volumes/ExternalDrive'
 export HLS_FOLDER='/tmp/hls'
 export HOST_NAME='example.com'
 
+# PostgreSQL
+export POSTGRESQL_HOST='127.0.0.1'
+export POSTGRESQL_PORT='5432'
+export POSTGRESQL_DB='music'
+export POSTGRESQL_USERNAME='example'
+export POSTGRESQL_PASSWORD='example'
+
 # Redis
 export REDIS_HOST='127.0.0.1'
 export REDIS_PORT='6379'
 export REDIS_DB='0'
+
+# Sunspot Solr
+export SUNSPOT_SOLR_HOSTNAME='127.0.0.1'
+export SUNSPOT_SOLR_PORT='8982'
+export SUNSPOT_SOLR_PATH='/solr/default'
