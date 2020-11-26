1e42d6a43acf29f43440f2cebd593809d9be94a3
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-11-26 19:27:29 +0100

    Update workers: Prefer ffmpeg over http streaming tools

--- app/workers/lame_worker.rb
+++ app/workers/lame_worker.rb
@@ -1,30 +1,24 @@
 class LameWorker
-
   include Sidekiq::Worker
 
   sidekiq_options :queue => :default, :retry => false, :backtrace => true
 
   def perform audio_file_id
     audio_file = AudioFile.find(audio_file_id).decorate
-    if !audio_file.file
-      begin
-        file_path = Shellwords.escape audio_file.decorate.public_path
-        temp_file = "/tmp/#{audio_file.id}.#{default_encoding}"
-        if ["AAC", "ALAC", "MP3"].include? audio_file.format_name
-          strip_metadata file_path, temp_file
-        elsif ["AIFF", "WAV"].include? audio_file.format_name
-          encode file_path, temp_file
-        end
-        audio_file.update! file: File.open(temp_file)
-      ensure
-        FileUtils.rm_f temp_file if temp_file && File.exists?(temp_file)
-      end
+    return if audio_file.file
+    file_path = Shellwords.escape audio_file.decorate.public_path
+    temp_file = '/tmp/%s.%s' % [audio_file.id, default_encoding]
+    if ['AAC', 'ALAC', 'MP3'].include? audio_file.format_name
+      strip_metadata file_path, temp_file
+    elsif ['AIFF', 'WAV'].include? audio_file.format_name
+      encode file_path, temp_file
     end
-  rescue Exception => e
-    Rails.logger.error e.inspect
+    audio_file.update! file: File.open(temp_file)
+  ensure
+    FileUtils.rm_f temp_file if temp_file && File.exists?(temp_file)
   end
 
-  private
+    private
 
   def default_encoding
     ALLOWED_AUDIO_FORMATS.deep_symbolize_keys.flat_map { |_, format| format[:extensions] if format[:default] }.compact.pop
@@ -35,12 +29,7 @@ class LameWorker
   end
 
   def encode source, destination
-    case default_encoding
-      when "mp3"
-        `lame --silent -b 320 -ms #{source} #{destination}`
-      when "aac"
-        `ffmpeg -i #{source} -c:a aac -strict -2 #{destination}`
-    end
+    `ffmpeg -i #{source} -c:a aac -strict -2 #{destination}`
   end
 
 end
--- app/workers/stream_worker.rb
+++ app/workers/stream_worker.rb
@@ -4,17 +4,17 @@ class StreamWorker
 
   sidekiq_options :queue => :default, :retry => false, :backtrace => true
 
-  def default_encoding
-    ALLOWED_AUDIO_FORMATS.deep_symbolize_keys.flat_map { |_, format| format[:extensions] if format[:default] }.compact.pop
-  end
-
   def perform audio_file_id, file_path
-    `ln -s #{file_path} #{HLS_FOLDER}/#{audio_file_id}.#{default_encoding}`
-    `mediafilesegmenter -b http://#{HOST_NAME}/hls -f #{HLS_FOLDER} #{HLS_FOLDER}/#{audio_file_id}.#{default_encoding} -B #{audio_file_id} -i #{audio_file_id}.m3u8`
-  rescue Exception => e
-    Rails.logger.error e.inspect
-  ensure
-    `rm #{HLS_FOLDER}/#{audio_file_id}.#{default_encoding}`
+    `ffmpeg -y \
+     -i #{file_path} \
+     -codec copy \
+     -map 0 \
+     -f segment \
+     -segment_time 10 \
+     -segment_format mpegts \
+     -segment_list "#{HLS_FOLDER}/#{audio_file_id}.m3u8" \
+     -segment_list_type m3u8 \
+     "#{HLS_FOLDER}/#{audio_file_id}_%d.ts"`
   end
 
 end
