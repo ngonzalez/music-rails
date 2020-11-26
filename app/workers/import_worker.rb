9348566711528de4ca804f7aa9711df3cff19fc8
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-11-26 22:10:40 +0100

    Update workers: Fix import

--- app/workers/import_worker.rb
+++ app/workers/import_worker.rb
@@ -9,12 +9,16 @@ class ImportWorker
   attr_accessor :music_folder
 
   def initialize options={}
-    @music_folder = MusicFolder.create! options.slice(*[:name, :folder, :subfolder, :source])
-    f = File::Stat.new music_folder.decorate.public_path
-    music_folder.update! folder_created_at: f.birthtime, folder_updated_at: f.mtime
+    @music_folder = MusicFolder.find_by name: options[:name]
+    @music_folder = MusicFolder.create! options.slice(*[:name, :folder, :subfolder, :source]) unless music_folder
   end
 
   def perform
+    f = File::Stat.new music_folder.decorate.public_path
+    if music_folder.folder_created_at != f.birthtime || music_folder.folder_updated_at != f.mtime
+      music_folder.update! folder_created_at: f.birthtime, folder_updated_at: f.mtime
+    end
+
     ALLOWED_AUDIO_FORMATS.flat_map { |_, format| format[:extensions] }.each do |format|
       list_files(music_folder.decorate.public_path, format) do |path, file_name|
         next if music_folder.audio_files.detect { |audio_file| audio_file.name == file_name }
