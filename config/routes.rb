b0da1d6ae87db8209f1a7ffc62439c19e6d15ee6
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-11-27 16:05:17 +0100

    Add default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root to: 'music_folders#index'
+
   resources :music_folders, only: [:index, :show]
 
   resources :audio_files, only: [:show]
