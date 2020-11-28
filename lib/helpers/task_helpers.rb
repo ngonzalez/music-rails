343e55fb2b02ebe1ad297b71dc437e0af02987f1
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-11-29 00:24:46 +0100

    Clean data.rake

--- lib/helpers/task_helpers.rb
+++ lib/helpers/task_helpers.rb
@@ -1,5 +1,26 @@
 module TaskHelpers
 
+  def import_music_folders
+    clear_deleted_folders
+    MusicFolder.find_each do |music_folder|
+      update_folder_path music_folder
+      update_folder_dates music_folder
+    end
+    import_folders
+    import_subfolders
+    update_music_folders_year
+    update_music_folders_formatted_name
+    update_music_folders_data_url
+    update_audio_files_data_url
+  end
+
+  def clear_database
+    ActiveRecord::Base.connection.execute "DELETE FROM #{MusicFolder.table_name} WHERE deleted_at IS NOT NULL"
+    ActiveRecord::Base.connection.execute "DELETE FROM #{Image.table_name} WHERE deleted_at IS NOT NULL"
+    ActiveRecord::Base.connection.execute "DELETE FROM #{AudioFile.table_name} WHERE deleted_at IS NOT NULL"
+    ActiveRecord::Base.connection.execute "DELETE FROM #{M3uFile.table_name} WHERE deleted_at IS NOT NULL"
+  end
+
   def clear_deleted_folders
     (MusicFolder.pluck(:folder).uniq - FOLDERS - FOLDERS_WITH_SUBFOLDERS).each do |folder|
       MusicFolder.where(folder: folder).destroy_all
--- lib/tasks/data.rake
+++ lib/tasks/data.rake
@@ -4,24 +4,11 @@ namespace :data do
 
   desc 'Import Music Folders'
   task update: :environment do
-    clear_deleted_folders
-    MusicFolder.find_each do |music_folder|
-      update_folder_path music_folder
-      update_folder_dates music_folder
-    end
-    import_folders
-    import_subfolders
-    update_music_folders_year
-    update_music_folders_formatted_name
-    update_music_folders_data_url
-    update_audio_files_data_url
+    import_music_folders
   end
 
   desc 'Clear database'
   task clear: :environment do
-    ActiveRecord::Base.connection.execute "DELETE FROM #{MusicFolder.table_name} WHERE deleted_at IS NOT NULL"
-    ActiveRecord::Base.connection.execute "DELETE FROM #{Image.table_name} WHERE deleted_at IS NOT NULL"
-    ActiveRecord::Base.connection.execute "DELETE FROM #{AudioFile.table_name} WHERE deleted_at IS NOT NULL"
-    ActiveRecord::Base.connection.execute "DELETE FROM #{M3uFile.table_name} WHERE deleted_at IS NOT NULL"
+    clear_database
   end
 end
