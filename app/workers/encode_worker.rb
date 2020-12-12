dd98846f0a2c2553a0be15f3bd64990e48931614
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-12-12 01:52:33 +0100

    Update workers

--- app/workers/encode_worker.rb
+++ app/workers/encode_worker.rb
@@ -8,7 +8,7 @@ class EncodeWorker
     audio_file = AudioFile.find(audio_file_id).decorate
     return if audio_file.file
     file = Down.download "http://#{BACKUP_SERVER_HOST}:#{BACKUP_SERVER_PORT}#{BACKUP_SERVER_PATH}/#{audio_file.path}"
-    temp_file = '/tmp/%s' % audio_file.id
+    temp_file = '/tmp/%s.%s' % [audio_file.id, audio_file.format_name.downcase]
     strip_metadata file.path, temp_file if ['AAC', 'ALAC', 'MP3'].include? audio_file.format_name
     encode file.path, temp_file if ['AIFF', 'WAV'].include? audio_file.format_name
     audio_file.update! file: File.open(temp_file)
