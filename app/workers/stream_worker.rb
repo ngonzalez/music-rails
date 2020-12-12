9981e2883145a4ec268bf85641a0895301000ea2
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-12-13 00:07:21 +0100

    StreamWorker: Use hls format

--- app/workers/stream_worker.rb
+++ app/workers/stream_worker.rb
@@ -6,20 +6,18 @@ class StreamWorker
 
   def perform audio_file_id, file_path
     `ffmpeg -y \
-     -i #{file_path} \
-     -codec copy \
-     -map 0 \
-     -loglevel 0 \
-     -f segment \
-     -segment_time 10 \
-     -segment_format mpegts \
-     -segment_list "#{APP_SERVER_TMP_PATH}/#{audio_file_id}.m3u8" \
-     -segment_list_type m3u8 \
-     "#{APP_SERVER_TMP_PATH}/#{audio_file_id}_%d.ts"`
+    -i #{file_path} \
+    -codec copy \
+    -map 0 \
+    -f hls \
+    -hls_time 10 \
+    -hls_playlist_type vod \
+    -hls_segment_filename "#{APP_SERVER_TMP_PATH}/#{audio_file_id}_%d.ts" \
+    "#{APP_SERVER_TMP_PATH}/#{audio_file_id}.m3u8"`
 
-     RedisDb.client.set "m3u8:#{audio_file_id}", 1
-   rescue => exception
-     Rails.logger.error exception
+    RedisDb.client.set "m3u8:#{audio_file_id}", 1
+  rescue => exception
+    Rails.logger.error exception
   end
 
 end
