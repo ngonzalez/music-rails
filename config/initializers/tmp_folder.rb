63db4fedc330e8c7e377254edc32442ba90d3146
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2020-12-13 01:07:10 +0100

    Update tmp_folder initializer

--- config/initializers/tmp_folder.rb
+++ config/initializers/tmp_folder.rb
@@ -1,5 +1,1 @@
-APP_SERVER_TMP_PATH = "/tmp/" + APP_SERVER_PATH
-
-unless File.exists? APP_SERVER_TMP_PATH
-  FileUtils.mkdir APP_SERVER_TMP_PATH
-end
+APP_SERVER_TMP_PATH = "/tmp" + APP_SERVER_PATH
