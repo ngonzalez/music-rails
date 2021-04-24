769b72af2d9e0d8c38fe71548256891e5806cad9
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2021-04-24 23:40:50 +0200

    Clear dragonfly from gcp

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -7,11 +7,6 @@ Dragonfly.app.configure do
   secret '8b00701357a0f7d45c5a5bedc64ba3a7db423a86a1b6ff2d1d752210f3212941'
 
   url_format "/media/:job/:id"
-
-  datastore :google,
-    project: 'hebe-001',
-    bucket: 'hebe-001.appspot.com',
-    keyfile: "#{[Dir.pwd, 'gcp.json'].join '/'}"
 end
 
 # Logger
